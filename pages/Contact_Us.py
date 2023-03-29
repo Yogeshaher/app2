c633c03ca0c7977c083ae0ef91f43ffa5ae07ed3
yogesh <ahersri@gmial.com>
2023-03-29 21:41:31 +0530

    app 2 final

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from send_email import send_email
 
+
 st.header("Contact me")
 
 with st.form(key="email_form"):
@@ -12,7 +13,6 @@ Subject : New email form {user_email}
 From : {user_email}
 {raw_message}
 """
-
     button = st.form_submit_button("Submit")
     print(button)
     if button:
